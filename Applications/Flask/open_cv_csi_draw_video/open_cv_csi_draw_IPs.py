774e1d60fc895f9861deb8fac6b9b0e1cbee1a6c
develtechmon <lukasjohnnytechmon@gmail.com>
2023-03-22 11:25:20 +0800

    +file housekeeping

--- Applications/Flask/open_cv_csi_draw_video/open_cv_csi_draw_IPs.py
+++ Applications/Flask/open_cv_csi_draw_video/open_cv_csi_draw_IPs.py
@@ -119,5 +119,5 @@ if __name__ == '__main__':
     process_thread = threading.Thread(target=captureFrames)
     process_thread.daemon = True
     process_thread.start()
-    app.run(host='10.60.130.138', port=80, threaded=True)
+    app.run(host='0.0.0.0', port=80, threaded=True)
     
