2206d7fed27e6b863db2da1a4bc2e8af2badc83f
develtechmon <lukasjohnnytechmon@gmail.com>
2023-04-17 18:31:57 +0800

    +modify the script to v1 drone

--- Projects/Autonomous_Human_Follower_Drone_v1/main_sequential_v1.py
+++ Projects/Autonomous_Human_Follower_Drone_v1/main_sequential_v1.py
@@ -1,5 +1,4 @@
 import cv2
-import sys
 import os
 import threading
 import state
@@ -9,10 +8,13 @@ import RPi.GPIO as GPIO
 from time import sleep,time
 from datetime import datetime
 from sys import exit
-import keyboard as kp
 
+from core import *
 from detect import *
-from camera import *
+
+#from camera import *
+from camera_non_thread import *
+
 from track import *
 from config import *
 from lidar import *
@@ -41,18 +43,16 @@ def takeoff():
 def search(id):
     start = time.time()
     drone.control_tab.stop_drone(altitude)
-    #while time.time() - start < state.get_time():
     while time.time() - start < 60:
         if (id == 1):
             state.set_system_state("track")
     state.set_system_state("land")
     
 def track(info):
-    #print(info[1])
     if (info[1]) != 0:
         state.set_airborne("on")
         det.track.trackobject(info,pid,pError,altitude)
-        
+
     else:
         state.set_system_state("search")
         state.set_time(60)
@@ -66,6 +66,34 @@ def record():
 def write(frame):
     writer.write(frame)
 
+def core_run(img,id,info):
+    if (state.get_system_state() == "takeoff"):
+        takeoff()
+
+    elif (state.get_system_state() == "search"):
+        search(id)
+
+    elif (state.get_system_state() == "track"):
+        track(info)
+
+    elif (state.get_system_state() == "land"):
+        writer.release()
+        drone.control_tab.land()
+        GPIO.output(buzzer,GPIO.HIGH)
+        sleep(1)
+        GPIO.output(buzzer,GPIO.LOW)
+        sleep(1)
+        
+    elif (state.get_system_state() == "end"):
+        state.set_system_state("takeoff")
+        state.set_airborne("off")
+            
+        print("Waiting to change to GUIDED Mode")
+            
+        while not drone.vehicle.mode.name == "GUIDED":
+            sleep(1)
+        writer = record()
+    
 if __name__ == "__main__":
     
     while True:
@@ -80,63 +108,49 @@ if __name__ == "__main__":
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(buzzer,GPIO.OUT, initial=GPIO.LOW)
 
-    # Initialize Camera
+    # Initialize CSI Camera
+    #cam = Camera()
+
+    # Initialize non CSI Camera
     cam = Camera()
     
-    # Intialize Recorder
+    #Intialize Recorder
     writer = record()
     
-    # Initialize Detector
+    #Initialize Detector
     det = Detect(cam,drone)
     
-    # Initialize Lidar
+    #Initialize Lidar
     lidar = Lidar(drone, altitude)
     lidar.start()
     
-    # Initialize state
+    #Initialize state
     state.set_system_state("takeoff")
     state.set_airborne("off")
     
-    while drone.is_active:     
-        
+    while drone.is_active:
+
         # Capture Image and perform detection
-        img, id, info = det.captureimage()
-           
-        if (state.get_system_state() == "takeoff"):
-            takeoff()
-     
-        # Search Image and Tracking
-        elif (state.get_system_state() == "search"):
-            search(id)
-            
-        elif (state.get_system_state() == "track"):
-            track(info)
-        
-        elif (state.get_system_state() == "land"):
-            writer.release()
-            drone.control_tab.land()
-            GPIO.output(buzzer,GPIO.HIGH)
-            sleep(1)
-            GPIO.output(buzzer,GPIO.LOW)
-            sleep(1)
-        
-        elif (state.get_system_state() == "end"):
-            state.set_system_state("takeoff")
-            state.set_airborne("off")
-            
-            print("Waiting to change to GUIDED Mode")
-            
-            while not drone.vehicle.mode.name == "GUIDED":
-                sleep(1)
-            writer = record()
-        
+        #img, id, info = det.captureimage()
+
+        # Capture Image using non-csi
+        img,id,info = det.captureimage_non_csi()
+
+        # Search and Track run using separate thread or class (?).
+        # Using Class Thread 
+        #core = Core(drone,altitude,det,pid,pError,writer,img,id,info)
+        #core.start()
+
+        # Using Thread
+        core_in = threading.Thread(target=core_run,daemon=True, args=(img,id,info))
+        core_in.start()
+                
         det.track.visualise(img)   
 
         cv2.imshow("Output",img)
         writer.write(img)
         
         if cv2.waitKey(1) & 0XFF == ord('q'):
-            #os.system("echo 2328 | sudo -S pkill -9 -f main.py")
             break
     
     writer.release()
